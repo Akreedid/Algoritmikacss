a96dd1f1e41e17e6396309be4f3d983d4376200f
Akreedid <onovolodskiy@gmail.com>
2024-09-16 14:05:47 +0500

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Destroy(gameObject);
+        //Destroy game object in the first time 
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ crickets.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crickets : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        // вывод в консоль повторяющее слово crickets 
+        print("crickets");
+    }
+}
